ee4e1d6ef9fdfa7bf5407b4c114d33a6d31d9e24
Philippe Casgrain <github@casgrain.com>
2012-10-04 00:29:49 -0400

    Améliorations au script

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ FONCTIONSR_SRC="/Users/philippe/Site ftp/Site ftp.umontreal.ca/labo/fonctions_r/
 FONCTIONSR_DST="/Volumes/bio$/Casgrain/prog/labo/fonctions_r/"
 LEGENDRE_SRC="/Users/philippe/Sites/legendre/"
 LEGENDRE_DST="/Volumes/bio$/legendre/"
-PDF_SRC="/Users/philippe/Site ftp/pdf legendre/"
+PDF_SRC="/Users/philippe/Site ftp/Reprints/"
 PDF_DST="/Volumes/bio$/legendre/reprints/"
 
 # Setup
@@ -19,11 +19,11 @@ fi
 
 # Sync
 echo "Fonctions R..."
-rsync -avz "$FONCTIONSR_SRC" "$FONCTIONSR_DST" --exclude ".DS_Store"
+rsync -avz "$FONCTIONSR_SRC" "$FONCTIONSR_DST" --exclude ".DS_Store" --exclude .git
 echo "Site Web Legendre..."
-rsync -avz "$LEGENDRE_SRC" "$LEGENDRE_DST" --exclude ".DS_Store"
+rsync -avz "$LEGENDRE_SRC" "$LEGENDRE_DST" --exclude ".DS_Store" --exclude .git
 echo "PDF Legendre..."
-rsync -avz "$PDF_SRC" "$PDF_DST" --exclude ".DS_Store"
+rsync -avz "$PDF_SRC" "$PDF_DST" --exclude ".DS_Store" --exclude .git
 
 # Teardown
 osascript -e 'tell application "Finder" to eject disk "bio$"'
